24cf552e9ed026c412fa4843c20fd59c9b47460a
Denidelito <cool.gsmes@yandex.ru>
2021-01-25 12:22:05 +0300

    add product list
    add img product and data list

--- src/data/products.js
+++ src/data/products.js
@@ -1,12 +1,92 @@
 export default [
   {
-    title: 'Радионяня Motorola MBP16',
-    img: 'img/radio.jpg',
-    price: '3 690',
+    title: 'iphone 3G',
+    img: 'img/iphone3G.jpg',
+    price: '3 700',
     colors: [
-      '#FFBE15',
-      '#8BE000',
-      '#FF6B00',
+      '#000000',
+      'rgba(114,114,114,0.47)',
+      '#f9f6ff',
+    ],
+  },
+  {
+    title: 'iphone 4',
+    img: 'img/iphone4.jpg',
+    price: '10 000',
+    colors: [
+      '#000000',
+      'rgba(114,114,114,0.47)',
+      '#f9f6ff',
+    ],
+  },
+  {
+    title: 'iphone 4s',
+    img: 'img/iphone4s.jpg',
+    price: '5 700',
+    colors: [
+      '#000000',
+      'rgba(114,114,114,0.47)',
+      '#f9f6ff',
+    ],
+  },
+  {
+    title: 'iphone 5',
+    img: 'img/iphone5.jpg',
+    price: '12 440',
+    colors: [
+      '#000000',
+      'rgba(114,114,114,0.47)',
+      '#f9f6ff',
+    ],
+  },
+  {
+    title: 'iphone 5s',
+    img: 'img/iphone5s.jpg',
+    price: '15 380',
+    colors: [
+      '#000000',
+      'rgba(114,114,114,0.47)',
+      '#f9f6ff',
+    ],
+  },
+  {
+    title: 'iphone SE',
+    img: 'img/iphoneSE.jpg',
+    price: '18 990',
+    colors: [
+      '#000000',
+      'rgba(114,114,114,0.47)',
+      '#f9f6ff',
+    ],
+  },
+  {
+    title: 'iphone 6',
+    img: 'img/iphone6.jpg',
+    price: '36 000',
+    colors: [
+      '#000000',
+      'rgba(114,114,114,0.47)',
+      '#f9f6ff',
+    ],
+  },
+  {
+    title: 'iphone 6s',
+    img: 'img/iphone6s.png',
+    price: '43 000',
+    colors: [
+      '#000000',
+      'rgba(114,114,114,0.47)',
+      '#f9f6ff',
+    ],
+  },
+  {
+    title: 'iphone 7',
+    img: 'img/iphone7.jpg',
+    price: '33 690',
+    colors: [
+      '#000000',
+      'rgba(114,114,114,0.47)',
+      '#f9f6ff',
     ],
   },
 ];
