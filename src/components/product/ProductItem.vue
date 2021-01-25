de938b1ced4b1fce4593ac90f0bfe7227847f429
Denidelito <cool.gsmes@yandex.ru>
2021-01-25 14:50:17 +0300

    ProductItem

--- src/components/product/ProductItem.vue
+++ src/components/product/ProductItem.vue
@@ -1,23 +1,21 @@
 <template>
-  <div>
-    <li class="catalog__item" >
-      <a class="catalog__pic" href="#">
-        <img :src="product.img" :alt="product.title">
+  <li class="catalog__item" >
+    <a class="catalog__pic" href="#">
+      <img :src="product.img" :alt="product.title">
+    </a>
+
+    <h3 class="catalog__title">
+      <a href="#">
+        {{product.title}}
       </a>
+    </h3>
 
-      <h3 class="catalog__title">
-        <a href="#">
-          {{product.title}}
-        </a>
-      </h3>
-
-      <span class="catalog__price">
+    <span class="catalog__price">
           {{product.price}}
       </span>
 
-      <ProductColor :colors="product.colors" :key-color="productIndex"/>
-    </li>
-  </div>
+    <ProductColor :colors="product.colors" :key-color="productIndex"/>
+  </li>
 </template>
 
 <script>
